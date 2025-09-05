a0ae9c5ff07a3154e293b15edf8744b80c3a0be9
Arun <arunsingh81302@gmail.com>
2025-09-05 17:09:23 +0530

    all done

---
+++ script.js
@@ -0,0 +1,69 @@
+let billinput = document.querySelector("#bill");
+let peopleinput = document.querySelector('#peopleinput');
+let buttons = document.querySelectorAll('.btn');
+let form = document.querySelector('form');
+let tipamount = document.querySelector('.tipamount');
+let tipperperson = document.querySelector('.tipperperson');
+let errorDiv = document.querySelector('.error'); // error div
+let reset=document.querySelector('.reset')
+
+let tip = 0;
+let billValue = 0;
+let numberofppl = 1;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+function calculate() {
+  if (billValue > 0 && numberofppl > 0 && tip > 0) {
+    let totalTip = billValue * (tip / 100);          // total tip
+    let tipEach = totalTip / numberofppl;            // tip per person
+    let totalPerPerson = (billValue + totalTip) / numberofppl; // total with tip
+
+    tipamount.textContent = `$${tipEach.toFixed(2)}`;      
+    tipperperson.textContent = `$${totalPerPerson.toFixed(2)}`;
+  } else {
+    tipamount.textContent = "$0.00";
+    tipperperson.textContent = "$0.00";
+  }
+}
+
+buttons.forEach(but => {
+  but.addEventListener('click', function () {
+    tip = parseInt(but.textContent);
+    calculate();
+  });
+});
+
+peopleinput.addEventListener('input', () => {
+  numberofppl = parseInt(peopleinput.value) || 0;
+
+  if (numberofppl === 0) {
+    peopleinput.style.border = "2px solid red";   // red border
+    errorDiv.style.visibility = "visible";        // show error
+  } else {
+    peopleinput.style.border = "none";            // reset border
+    errorDiv.style.visibility = "hidden";         // hide error
+    calculate();
+  }
+});
+
+billinput.addEventListener('input', () => {
+  billValue = parseFloat(billinput.value) || 0;
+  calculate();
+});
+reset.addEventListener("click", () => {
+      tip = 0;
+      billValue = 0;
+      numberofppl = 1;
+      
+      billinput.value=""
+      peopleinput.value=""
+      
+      tipamount.textContent = "$0.00";
+      tipperperson.textContent = "$0.00";
+    
+      peopleinput.style.border = "none";
+      document.querySelector(".error").style.visibility = "hidden";
+    });
